500e31378c6e78136b46a510966735f4971b3f3c
Najla Parker <najla08@gmail.com>
2021-11-20 22:06:45 -0700

    Revised local storage in script.js and added functionality for time-block color coding

--- script.js
+++ script.js
@@ -1,26 +1,51 @@
-var hour9 = $("#9");
-var hour10 = $("#10");
-var hour11 = $("#11");
-var hour12 = $("#12");
-var hour1 = $("#13");
-var hour2 = $("#14");
-var hour3 = $("#15");
-var hour4 = $("#16");
-var hour5 = $("#17");
-var time = moment();
+let time = moment();
 
 function plannerSetup() {
+  // add current date and time to header of planner
   $("#currentDay").text(moment().format("dddd, MMMM Do YYYY [at] h:mm A z"));
 
   $(".time-block").each(function () {
-    var id = $(this).attr("id");
-    var schedule = localStorage.getItem(id);
+    let id = $(this).attr("id");
+    let LS = localStorage.getItem(id);
 
-    if (schedule !== null) {
-      $(this).children(".schedule").val(schedule);
+    if (LS !== null) {
+      $(this).children(".planEvent").val(LS);
+    }
+  });
+}
+plannerSetup();
+let addBtn = $(".addBtn");
+
+$(".addBtn").on("click", function () {
+  let time = $(this).parent().attr("id");
+  var scheduleEntry = $(this).siblings(".planEvent").val();
+  localStorage.setItem(time, scheduleEntry);
+});
+
+function pastPresentFuture() {
+  hour = time.hours();
+
+  $(".time-block").each(function () {
+    let currentHour = parseInt($(this).attr("id"));
+    // Add class based on relation to current hour and class will be styled with appropriate color coding.
+    if (currentHour > hour) {
+      $(this).addClass("future");
+    } else if (currentHour === hour) {
+      $(this).addClass("present");
+    } else {
+      $(this).addClass("past");
     }
   });
 }
 
-plannerSetup();
-var addBtn = $(".addBtn");
+$("9.planEvent").val(localStorage.getItem("9"));
+$("10.planEvent").val(localStorage.getItem("10"));
+$("11.planEvent").val(localStorage.getItem("11"));
+$("12.planEvent").val(localStorage.getItem("12"));
+$("13.planEvent").val(localStorage.getItem("13"));
+$("14.planEvent").val(localStorage.getItem("14"));
+$("15.planEvent").val(localStorage.getItem("15"));
+$("16.planEvent").val(localStorage.getItem("16"));
+$("17.planEvent").val(localStorage.getItem("17"));
+// When page is loaded call this function
+pastPresentFuture();
